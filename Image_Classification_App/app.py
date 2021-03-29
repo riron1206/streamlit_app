2373c419f5ee218acc24844156260e35f954fac3
riron1206 <rironyokoi@gmail.com>
2021-03-30 01:37:17 +0900

    add py from my_PC

--- Image_Classification_App/app.py
+++ Image_Classification_App/app.py
@@ -5,15 +5,20 @@ https://towardsdatascience.com/create-an-image-classification-web-app-using-pyto
 Usage:
     $ streamlit run ./app.py
 """
+import pandas as pd
 import streamlit as st
 import torch
 from torchvision import models, transforms
 from PIL import Image
 
 
-def predict(image_path):
-    resnet = models.resnet101(pretrained=True)
+@st.cache(allow_output_mutation=True)
+def load_model():
+    model = models.resnet101(pretrained=True)
+    return model
 
+
+def preprocessing_image(image_pil_array: "PIL.Image"):
     # https://pytorch.org/docs/stable/torchvision/models.html
     transform = transforms.Compose(
         [
@@ -23,10 +28,15 @@ def predict(image_path):
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
     )
+    batch_t = torch.unsqueeze(transform(image_pil_array), 0)
+    return batch_t
 
+
+def predict(image_path):
     img = Image.open(image_path)
-    batch_t = torch.unsqueeze(transform(img), 0)
+    batch_t = preprocessing_image(img)
 
+    resnet = load_model()
     resnet.eval()
     out = resnet(batch_t)
 
@@ -48,6 +58,7 @@ def main():
     if file_up is not None:
         image = Image.open(file_up)
         st.image(image, caption="Uploaded Image.", use_column_width=True)
+
         st.write("")
         st.write("Just a second...")
         labels = predict(file_up)
@@ -55,6 +66,9 @@ def main():
         # print out the top 5 prediction labels with scores
         for i in labels:
             st.write("Prediction (index, name)", i[0], ",   Score: ", round(i[1], 2))
+    else:
+        img_url = "https://github.com/riron1206/streamlit_app/blob/master/Image_Classification_App/image/dog.jpg?raw=true"
+        st.image(img_url, caption="Sample Image.", use_column_width=True)
 
 
 if __name__ == "__main__":
