552c13c848270a24faa55f568d80d1462786b271
riron1206 <rironyokoi@gmail.com>
2021-04-02 00:43:23 +0900

    add py from my_PC

--- Image_Classification_App/README.md
+++ Image_Classification_App/README.md
@@ -1,1 +1,6 @@
 ![gif](app_run.gif)
+
+---------------------------------------------
+
+##### References:
+- https://github.com/1Konny/gradcam_plus_plus-pytorch
--- Image_Classification_App/app.py
+++ Image_Classification_App/app.py
@@ -1,21 +1,31 @@
 """
-画像を Streamlit 上で表示して、学習済みのresnet101(Pytorch)で分類
+画像を Streamlit 上で表示して、学習済みの mobilenet_v2(Pytorch)で分類
 https://towardsdatascience.com/create-an-image-classification-web-app-using-pytorch-and-streamlit-f043ddf00c24
 
 Usage:
     $ conda activate lightning
     $ streamlit run ./app.py
 """
+import sys
+import cv2
+import numpy as np
 import pandas as pd
 import streamlit as st
 import torch
 from torchvision import models, transforms
 from PIL import Image
 
+sys.path.append("./gradcam_plus_plus-pytorch")
+from gradcam.utils import visualize_cam
+from gradcam import GradCAM, GradCAMpp
+import albumentations as A
+from albumentations.pytorch.transforms import ToTensorV2
+
 
 @st.cache(allow_output_mutation=True)
 def load_model():
-    model = models.resnet101(pretrained=True)
+    # model = models.resnet101(pretrained=True)
+    model = models.mobilenet_v2(pretrained=True)
     return model
 
 
@@ -23,8 +33,9 @@ def preprocessing_image(image_pil_array: "PIL.Image"):
     # https://pytorch.org/docs/stable/torchvision/models.html
     transform = transforms.Compose(
         [
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
+            # transforms.Resize(256),
+            # transforms.CenterCrop(224),
+            transforms.Resize(224),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ]
@@ -37,16 +48,64 @@ def predict(image_path):
     img = Image.open(image_path)
     batch_t = preprocessing_image(img)
 
-    resnet = load_model()
-    resnet.eval()
-    out = resnet(batch_t)
+    model = load_model()
+    model.eval()
+    out = model(batch_t)
 
     with open("imagenet_classes.txt") as f:
         classes = [line.strip() for line in f.readlines()]
 
     prob = torch.nn.functional.softmax(out, dim=1)[0] * 100
     _, indices = torch.sort(out, descending=True)
-    return [(classes[idx], prob[idx].item()) for idx in indices[0][:5]]
+    top5 = [(classes[idx], prob[idx].item()) for idx in indices[0][:5]]
+
+    result_pp = _gradcam(model, img)
+
+    return top5, result_pp
+
+
+def _gradcam(model, image_pil_array: "PIL.Image"):
+    """grad-cam++"""
+    target_layer = model.features  # mobilenet_v2
+
+    img_cv2 = pil2cv(image_pil_array)
+
+    img = img_cv2 / 255
+    a_transform = A.Compose([A.Resize(224, 224, p=1.0), ToTensorV2(p=1.0)], p=1.0)
+    img = a_transform(image=img)["image"].unsqueeze(0)
+
+    a_transform = A.Compose(
+        [
+            A.Resize(224, 224, p=1.0),
+            A.Normalize(
+                mean=[0.485, 0.456, 0.406],
+                std=[0.229, 0.224, 0.225],
+                max_pixel_value=255.0,
+                p=1.0,
+            ),
+            ToTensorV2(p=1.0),
+        ],
+        p=1.0,
+    )
+    batch_t = a_transform(image=img_cv2)["image"].unsqueeze(0)
+
+    gradcam_pp = GradCAMpp(model, target_layer)
+    mask_pp, logit = gradcam_pp(batch_t)
+    heatmap_pp, result_pp = visualize_cam(mask_pp, img)
+    return result_pp
+
+
+def pil2cv(image):
+    """ PIL型 -> OpenCV型
+    https://qiita.com/derodero24/items/f22c22b22451609908ee"""
+    new_image = np.array(image, dtype=np.uint8)
+    if new_image.ndim == 2:  # モノクロ
+        pass
+    elif new_image.shape[2] == 3:  # カラー
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_RGB2BGR)
+    elif new_image.shape[2] == 4:  # 透過
+        new_image = cv2.cvtColor(new_image, cv2.COLOR_RGBA2BGRA)
+    return new_image
 
 
 def main():
@@ -62,7 +121,12 @@ def main():
 
         st.write("")
         st.write("Just a second...")
-        labels = predict(file_up)
+        labels, result_pp = predict(file_up)
+        st.image(
+            result_pp.numpy().transpose(1, 2, 0),
+            caption="Model Attention(Grad-CAM++).",
+            use_column_width=True,
+        )
 
         # print out the top 5 prediction labels with scores
         for i in labels:
